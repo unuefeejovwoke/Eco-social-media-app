a6f076fd62ec841efaa2c12ce783b1faef17a0af
unuefeejovwoke <vuwill7114@gmail.com>
2023-07-14 12:18:55 -0700

    emailauth done

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -112,6 +112,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTHENTICATION_BACKENDS = [
+ 'django.contrib.auth.backends.ModelBackend',
+ 'account.authentication.EmailAuthBackend',
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
